8e0e2aa068e84a6b5212293d123c4d89e8582a2c
cocosmihai10 <cocosmihai10@gmail.com>
2022-11-06 18:33:02 +0200

    working on web-scrapping with cheerio

--- back-end/index.js
+++ back-end/index.js
@@ -2,9 +2,16 @@ const express = require("express")
 const app = express()
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
+const cors = require('cors')
+const axios = require('axios');
+const cheerio  = require("cheerio");
+const pretty = require("pretty")
 
+app.use(cors())
 const connectionURl = 'mongodb://127.0.0.1:27017';
 const database = "myMDB"
+let db; 
+const elements = []
 
 mongoClient.connect(
     connectionURl,
@@ -15,7 +22,7 @@ mongoClient.connect(
             return
         }
 
-        const db = cli.db(database)
+        db = cli.db(database)
 
         // db.collection('posts').insertOne({
         //     name:"Tudor",
@@ -31,6 +38,58 @@ mongoClient.connect(
 
 app.use(express.json())
 
-app.listen(3000, () => {
+app.post("/post_destination", (req, res) => {
+    console.log("Endpoint available")
+    db.collection("posts").insertOne({
+        name:req.body.name,
+        location:req.body.location,
+        price:req.body.price
+    })
+})
+
+app.get("/get_posts", (req, res) => {
+    axios.get("https://sp.booking.com/searchresults.en-gb.html?aid=318615&label=Catch_All-EN-131006968001-bPiN0WYm7x7ddzlXSroMLwS548793046706%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atidsa-1642216383571%3Alp1011837%3Ali%3Adec%3Adm&sid=8e1d1ad3cd8d7f925a3288aef1318628&city=-1251042;from_idr=1&ilp=1&d_dcp=1")
+    .then((data) => {
+        const $ = cheerio.load(data.data)
+        $('.ef8295f3e6').each(function(i, elem){
+            console.log("text " + $(this).text())
+            const url_text = $(this).children("div").children(".dd023375f5").children("h3").children("a").children(".fcab3ed991").text();
+            const url_href = $(this).children("div").children(".dd023375f5").children("h3").children("a").attr("href")
+            const description = $(this).children(".d8eab2cf7f").text()
+            description.length && 
+            elements.push({
+                url_text,
+                url_href,
+                description: description
+            })
+        })
+        //console.log("elements: " + elements)
+        //res.json(elements)
+        res.send(elements)
+        //const link = html(".a4225678b2").children("a").text();
+        // console.log("- url: " + link)
+        // console.log("- description: " + pretty(description))
+        // elements.push({
+        //     link,
+        //     description
+        // })
+        // html('span span').each((idx, el) => {
+        //    // const text =html(el).text()
+        //    const href = html(el).attr('href')
+        //    const text = html(el).text()
+        //     // console.log(html(el).children(".flex").children(".px-2").children("section").children('h2').children("button").children("span").children())
+        //     elements.push({
+        //         url:text
+        //     })
+        // })
+    }).catch(err => console.log("error: " + err))
+    // db.collection("posts").find().toArray(function(err, result){
+    //         res.send(result)
+    // })
+})
+
+
+
+app.listen(3001, () => {
     console.log("Server started :)")
 })
