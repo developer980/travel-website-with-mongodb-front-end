2e2a16c4055d9fae5008aa2d9bd1c4c78bf8e5cb
Tudor Cernat <tudordin2002@gmail.com>
2023-01-14 01:40:40 +0200

    working on user authentication

--- back-end/index.js
+++ back-end/index.js
@@ -9,7 +9,8 @@ const pretty = require("pretty");
 const e = require("express");
 const src = require("./function1");
 const { resolve } = require("path");
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
+const { cursorTo } = require("readline");
 
 app.use(cors())
 const connectionURl = 'mongodb://127.0.0.1:27017';
@@ -56,11 +57,13 @@ app.post("/post_user", (req, res) => {
     const email = req.body.email
     const username = req.body.username
     const password = req.body.password
+    const token = req.body.token
     console.log("Endpoint available")
     db.collection("pending_users").insertOne({
         username,
         email,
-        password
+        password,
+        token,
     })
     const mailOptions = {
         from: "confirm.test@outlook.com",
@@ -68,7 +71,7 @@ app.post("/post_user", (req, res) => {
         subject: "Email confirmation",
         html: `<div>
             <b>Please verify your email</b>
-            <a href = "http://localhost:3000"></a>
+            <a href = "http://localhost:3000/confirm_${token}">Link</a>
         </div>`
     }
     transport.sendMail(mailOptions, (err, res) => {
@@ -76,24 +79,30 @@ app.post("/post_user", (req, res) => {
     })
 })
 
-//https://sp.booking.com/searchresults.en-gb.html?aid=318615&label=Catch_All-EN-131006968001-bPiN0WYm7x7ddzlXSroMLwS548793046706%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atidsa-1642216383571%3Alp1011837%3Ali%3Adec%3Adm&sid=8e1d1ad3cd8d7f925a3288aef1318628&city=-1251042;from_idr=1&ilp=1&d_dcp=1
+app.post("/verify_token", (req, res) => {
+    const token = req.body.token
+    console.log(token)
+    const response = []
+    console.log("verifying...")
+    const result = db.collection("pending_users").find({
+        token:token
+    })
 
-//https://www.booking.com/searchresults.ro.html?label=munte-3tfS0Uzd3aQVUzbZzN0zKQS500104469167%3Apl%3Ata%3Ap12%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-13155828247%3Alp1011843%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YURcq_26dhSxQ4lAWSVBlCA&sid=8e1d1ad3cd8d7f925a3288aef1318628&aid=319856&city=-1965903&redirected=1&redirected_from_city=1&source=city&offset=25
-// https://www.booking.com/searchresults.ro.html?aid=341360&label=metatrivago-hotel-41057_xqdz-eae923_los-1_nrm-1_gstadt-2_gstkid-0_curr-ron_lang-ro_mcid-10_dev-dsk_losb-1_bw-7_bwb-5_pg-0_dd-0_gsb-2_sl-0_w-0_tstar-0_trat-0_tprc-tprcd_tamnt-0_cod-1669756450_trvref-a4790a42-7a87-44b8-9d58-e473c81cfca3&sid=8e1d1ad3cd8d7f925a3288aef1318628&checkin=2022-12-06&checkout=2022-12-07&city=-1153951&group_adults=2&group_children=0&highlighted_hotels=41057&hlrd=with_dates&keep_landing=1&no_rooms=1&redirected=1&selected_currency=RON&show_room=4105732_118364697_0_2_0&source=hotel&trv_curr=RON&trv_dp=616&utm_campaign=ro&utm_content=los-1_nrm-1_gstadt-2_gstkid-0_curr-ron_lang-ro_mcid-10_dev-dsk_losb-1_bw-7_bwb-5_pg-0_dd-0_gsb-2_sl-0_w-0_tstar-0_trat-0_tprc-tprcd_tamnt-0_cod-1669756450_trvref-a4790a42-7a87-44b8-9d58-e473c81cfca3&utm_medium=meta&utm_source=metatrivago&utm_term=hotel-41057&room1=A,A,;
-// https://www.booking.com/searchresults.ro.html?ss=Sibiu&ssne=Bucure%C8%99ti&ssne_untouched=Bucure%C8%99ti&highlighted_hotels=41057&efdco=1&label=metatrivago-hotel-41057_xqdz-eae923_los-1_nrm-1_gstadt-2_gstkid-0_curr-ron_lang-ro_mcid-10_dev-dsk_losb-1_bw-7_bwb-5_pg-0_dd-0_gsb-2_sl-0_w-0_tstar-0_trat-0_tprc-tprcd_tamnt-0_cod-1669756450_trvref-a4790a42-7a87-44b8-9d58-e473c81cfca3&sid=8e1d1ad3cd8d7f925a3288aef1318628&aid=341360&lang=ro&sb=1&src_elem=sb&src=searchresults&dest_id=-1170221&dest_type=city&ac_position=0&ac_click_type=b&ac_langcode=ro&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=faae95549ff2004a&ac_meta=GhBmYWFlOTU1NDlmZjIwMDRhIAAoATICcm86AlNpQABKAFAA&checkin=2022-12-06&checkout=2022-12-07&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure
-
-// https://www.booking.com/searchresults.ro.html?aid=318615&label=New_Romanian_RO_RO_27026375305-LNg_O2KjtrOYqfazt2IDdgS217247418951%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi10681170931%3Atidsa-301303784602%3Alp1011843%3Ali%3Adec%3Adm&sid=8e1d1ad3cd8d7f925a3288aef1318628&checkin_monthday=04&checkin_year_month=2022-12&checkout_monthday=05&checkout_year_month=2022-12&dest_id=-1153951&dest_type=city&from_history=1&group_adults=2&group_children=0&no_rooms=1&si=ad&si=ai&si=ci&si=co&si=di&si=la&si=re&&sh_position=1
-
-// for filters: https://www.booking.com/searchresults.ro.html?ss=Tulcea&checkin=2022-12-09&checkout=2022-12-20&group_adults=1&no_rooms=1&group_children=2
-       
-//https://www.booking.com/searchresults.ro.html?ss=Bucuresti&checkIn=2022-11-20&checkOut=2022-11-23
-
-// 2nd link: https://www.trip.com/hotels/list?city=0&cityName=Tokyo
-
-// 2nd link: https://www.expedia.com/Hotel-Search?adults=2&destination=Tulcea
-
-
-//https://www.zenhotels.com/hotel/romania/arad/?q=6158&dates=13.01.2023-30.01.2023&guests=2&price=one&sid=9c1da76f-3bb1-4b9b-a9f6-8f001d2eb5f1
+    result.forEach(function (result, err) {
+        err && console.log(err)
+        console.log(result)
+            response.push(result)
+            //res.send(result)
+    }, () => {
+       // db.close()
+        console.log(response[0])
+        response[0] && res.send(response[0])
+        db.collection("pending_users").deleteOne({token:token})
+    })
+    // cursorTo.array.forEach(element => {
+    //     console.log("element" + element)
+    // });
+})
 
 app.post("/get_posts", (req, res) => {
     let keyWord = req.body.keyWord
@@ -115,32 +124,6 @@ app.post("/get_posts", (req, res) => {
     elements = []
     elements1 = []
     elements2 = []
-
-    let array_of_arrays = []
-
-    //console.log(`https://www.booking.com/searchresults.ro.html?ss=${keyWord}${"&checkin=" + req.body.parameters.checkIn.year + "-" + req.body.parameters.checkIn.month + "-" + req.body.parameters.checkIn.day}${"&checkout=" + req.body.parameters.checkIn.year + "-" + req.body.parameters.checkIn.month + "-" + req.body.parameters.checkIn.day}`)
-
-    //console.log(`https://www.expedia.com/Hotel-Search?destination=${keyWord}&startDate=${req.body.parameters.checkIn}${"&endDate=" + req.body.parameters.checkOut}`)
-    
-   // console.log(`https://www.zenhotels.com/hotel/romania/${keyWord}/?q=6158&dates=${checkIn.day}.${checkIn.month}.${checkIn.year}-${checkOut.day}.${checkOut.month}.${checkOut.year}`)
-
-   // const promise0 = new Promise((resolve) => {
-        // axios.get(`https://www.zenhotels.com/hotel/romania/${keyWord}/?q=6158&dates=${checkIn.day}.${checkIn.month}.${checkIn.year}-${checkOut.day}.${checkOut.month}.${checkOut.year}`).then((data) => {
-        //     const $ = cheerio.load(data.data)
-        //     $('.zen-hotelcard').each(function () {
-        //         // const name = $(this).find(".uitk-layout-flex").find("h2")
-        //         // const price = $(this).find(".uitk-layout-flex").find(".uitk-layout-flex").find(".uitk-layout-flex").children(".uitk-layout-flex").children(".uitk-type-200")
-
-        //         //     elements1.push({
-        //         //         name: name.text(),
-        //         //         price: price.text().split(" ")[0].substring(1) * 4.64
-        //         //     })
-        //         console.log("html " + pretty($(this).html()))
-        //     })
-        //     // console.log("l2 " + elements1.length)
-        //     // resolve(elements1)
-        // })
-    //})
 
     console.log(`https://www.expedia.com/Hotel-Search?destination=${keyWord}&startDate=${checkIn.year + "-" + checkIn.month + "-" + checkIn.day}${"&endDate=" + checkOut.year + "-" + checkOut.month + "-" + checkOut.day}`)
 
