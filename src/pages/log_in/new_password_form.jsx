32b72cb687d1d851e8c8fea461966f4bb7f72941
developer980 <tudordin2002@gmail.com>
2023-02-11 01:28:15 +0200

    working on password recovery

---
+++ src/pages/log_in/new_password_form.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from "axios"
+import ls from "localstorage-slim"
+import Layout from '../../components/layout/layout'
+
+export default function New_password_form() {
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState(0)
+    axios.post('https://mydestinationapp.onrender.com/verify_token', {
+        email:ls.get('eml')
+    }).then((data) => {
+        data.data && setMessage(1)
+    })
+
+    return (
+        <Layout>
+            <form className='user-from'>
+                {
+                    message ?
+                    <div>
+                        <span className='field-name'>Insert your new password</span>
+                        <input onChange = {(e) => setPassword(e.target.value)} id = "name" type="password" placeholder='Insert password'/>
+                        
+                        <button onClick={() => {
+                            axios.post('https://mydestinationapp.onrender.com/reset_password', {
+                                mail:ls.get('eml'),
+                                password
+                            }).then((data) => {
+                                data.data && setMessage(1)
+                            })
+                                
+                        }}>
+                        </button>
+                    </div> :
+                    <div className='negative-message'>
+                        Token invalid or has expired          
+                    </div>      
+                }
+            
+            </form>
+        </Layout>
+  )
+}
