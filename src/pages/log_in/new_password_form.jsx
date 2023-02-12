d45a0442cf8220425c787d032464547f3685728c
developer980 <tudordin2002@gmail.com>
2023-02-13 01:46:09 +0200

    minor fixes

--- src/pages/log_in/new_password_form.jsx
+++ src/pages/log_in/new_password_form.jsx
@@ -25,8 +25,8 @@ export default function New_password_form() {
         console.log(data.data)
         if (data.data) {
             ls.remove("dest_eml")
-            // setMessage(1)
-            console.log(data.data)
+            setMessage(1)
+           // console.log(data.data)
         }
     })
 
